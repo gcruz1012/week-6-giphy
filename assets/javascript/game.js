67e5418405a4b8157f98c1f293536856d8fddfd3
Guillermo Cruz <gcruz1012@Guillermos-MacBook-Pro.local>
2017-12-20 12:41:44 -0500

    pausable Gifs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,124 @@
 var animals=["cat", "dog", "mouse", "bird", "zebra", "lion","horse", "tiger", "pig"];
 
-
 for(var i=0;i<animals.length;i++){
+
 	var animalButton = $("<button>");
+
 	animalButton.attr("id",animals[i]);
+
 	animalButton.html(animals[i]);
 
 	$("#animalButtons").append(animalButton)
 
+	};
 
-};
+
 function all(){
-   $("button").on("click", function clicked() {
-     var it = $(this).attr("id");
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + it + "&api_key=slH3G7Y5Q8II7nD8yUF1iGfmKRMvqQVL&limit=10";
 
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
+	$("button").on("click", function clicked() {
 
-        .done(function (response) {
-          
-                if (!$("#animals").is(":empty")){
-      			$("#animals").html("");
-     		 }
-     		 else{
-     		 	var results = response.data;
-          for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div class='item'>");
+		var it = $(this).attr("id");
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + it + "&api_key=slH3G7Y5Q8II7nD8yUF1iGfmKRMvqQVL&limit=10";
+	
 
-            var rating = results[i].rating;
+		$.ajax({
+		url: queryURL,
+		method: "GET"
+		})
 
-            var p = $("<p>").text("Rating: " + rating);
 
-            var animalImage = $("<img>");
-            animalImage.attr("src", results[i].images.fixed_height.url);
+	.done(function (response) {
+	
+		if (!$("#animals").is(":empty")){
 
-            gifDiv.prepend(p);
-            gifDiv.prepend(animalImage);
+		$("#animals").html("");
 
-            $("#animals").append(gifDiv);
-          }
-      };
-        });
-    
-});
+		}
+
+
+		else{
+
+			var results = response.data;
+
+			for (var j = 0; j < results.length; j++) {
+
+				var gifDiv = $("<div>");
+
+				var rating = results[j].rating;
+
+				var p = $("<p>").text("Rating: " + rating);
+
+				var animalImage = $("<img class='item'>");
+
+				animalImage.attr("src", results[j].images.fixed_height_still.url);
+
+				animalImage.attr("data-still",results[j].images.fixed_height_still.url);
+
+				animalImage.attr("data-animate",results[j].images.fixed_height.url)
+
+				animalImage.attr("data-state","still");
+				
+				console.log(animalImage);
+
+				gifDiv.prepend(p);
+
+				gifDiv.prepend(animalImage);
+
+				$("#animals").append(gifDiv);
+			}
+
+			$(".item").on("click", function() {
+
+				var state=$(this).attr("data-state");
+				// console.log(state);
+				var animate=$(this).attr("data-animate");
+
+				var still=$(this).attr("data-still");
+
+				if(state==="still"){
+
+					$(this).attr("src", animate);
+
+					$(this).attr("data-state","animate");
+
+					console.log(this);
+				}
+
+
+				else{
+					$(this).attr("src", still);
+
+					$(this).attr("data-state", "still");
+				}
+
+
+			});
+
+			
+		};
+
+	});
+			
+	});
 };
-   $("#addAnimal").on("click",function(event){
-	event.preventDefault();
-var newAnimal=$("#animal-input").val().trim();
-console.log(newAnimal);
-animals.push(newAnimal);
-	var animalButton = $("<button>");
-	animalButton.attr("id",animals[animals.length-1]);
-	animalButton.html(animals[animals.length-1]);
+			$("#addAnimal").on("click",function(event){
 
-	$("#animalButtons").append(animalButton)
+				event.preventDefault();
+
+				var newAnimal=$("#animal-input").val().trim();
+
+				console.log(newAnimal);
+			
+				animals.push(newAnimal);
+			
+				var animalButton = $("<button>");
+			
+				animalButton.attr("id",animals[animals.length-1]);
+			
+				animalButton.html(animals[animals.length-1]);
+				
+				$("#animalButtons").append(animalButton)
+				
+				all();
+			});
 all();
-});
-   all();
